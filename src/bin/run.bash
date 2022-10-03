a49ec67d1645f0ded17d88488bbebf7a09ab3668
Jaid <jaid.jsx@gmail.com>
2022-10-04 01:06:42 +0200

    Finished script
    Signed-off-by: Jaid <jaid.jsx@gmail.com>

--- src/bin/run.bash
+++ src/bin/run.bash
@@ -1,17 +1,55 @@
 #!/usr/bin/env bash
 set -o errexit
 
-: "${amount:=1}"
-: "${currency:=BTC}"
-: "${prefix:=1Test}"
+if [[ -z $currency ]]; then
+  printf >&2 'Variable $currency not existing\n'
+  exit 1
+fi
 
-outputFolder=/results/$currency/$prefix
-outputFile=$outputFolder/catches.csv
+if [[ -z $prefix ]]; then
+  printf >&2 'Variable $prefix not existing\n'
+  exit 1
+fi
+
+inputFile=/results/input.lines
+outputFolder=/results/$currency
+outputFile=$outputFolder/catches.txt
 logFile=$outputFolder/vanitygen.log
 
 if [[ -f $outputFile ]]; then
-  printf '\n-------------------------------------------------------------------------------\n' >>$outputFile
+  printf '\n╞═════════════════════════════════════════════════════════════════════════════╡\n' >>"$outputFile"
 else
-  mkdir --parents $outputFolder
+  mkdir --parents "$outputFolder"
 fi
-./vanitygen++ -v -C "$currency" "$prefix" -o $outputFile |& tee --append $logFile
+if [[ -f $logFile ]]; then
+  printf '\n╞═════════════════════════════════════════════════════════════════════════════╡\n' >>"$logFile"
+fi
+
+if [[ -f $inputFile ]]; then
+  truncate --size 0 $inputFile
+fi
+IFS=' ' read -r -a entries <<<"$prefix"
+for entry in "${entries[@]}"; do
+  printf '%s\n' "$entry" >>$inputFile
+done
+
+function main() {
+  declare -a arguments=()
+  arguments+=(-v) # verbose
+  arguments+=(-f) # pattern file
+  arguments+=("$inputFile")
+  arguments+=(-C) # currency
+  arguments+=("$currency")
+  arguments+=(-o) # output file
+  arguments+=("$outputFile")
+
+  if [[ -z $amount ]]; then
+    arguments+=(-a)
+    arguments+=("$amount")
+  else
+    arguments+=(-k)
+  fi
+  ./vanitygen++ "${arguments[@]}"
+}
+
+main |& tee --append "$logFile"
