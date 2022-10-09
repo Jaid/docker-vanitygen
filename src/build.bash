5bbc2a96fea3f472870ba19a3efe249589c4d5a0
Jaid <jaid.jsx@gmail.com>
2022-10-09 13:34:03 +0200

    First working version
    Signed-off-by: Jaid <jaid.jsx@gmail.com>

---
+++ src/build.bash
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+set -o errexit
+
+repoSha=303df78232765713bbb459ffb0d50a930e4a7304
+
+DEBIAN_FRONTEND=noninteractive apt-get --option Acquire::Retries=60 --option Acquire::http::Timeout=180 --yes update
+DEBIAN_FRONTEND=noninteractive apt-get --option Acquire::Retries=60 --option Acquire::http::Timeout=180 --yes upgrade
+DEBIAN_FRONTEND=noninteractive apt-get --option Acquire::Retries=60 --option Acquire::http::Timeout=180 --yes install curl
+DEBIAN_FRONTEND=noninteractive apt-get --option Acquire::Retries=60 --option Acquire::http::Timeout=180 --yes install build-essential libssl-dev libpcre3-dev libcurl4-openssl-dev
+curl --location --fail --show-error https://github.com/10gic/vanitygen-plusplus/archive/$repoSha.tar.gz --output vanitygen-plusplus.tar.gz
+tar -x -z -f vanitygen-plusplus.tar.gz
+mv vanitygen-plusplus-$repoSha vanitygen-plusplus
+make --directory vanitygen-plusplus
+mv vanitygen-plusplus/vanitygen++ .
+mv vanitygen-plusplus/base58prefix.txt .
+rm -rf vanitygen-plusplus
+rm vanitygen-plusplus.tar.gz
---
+++ src/content/bin/prepare.bash
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+
+DEBIAN_FRONTEND=noninteractive apt-get --option Acquire::Retries=60 --option Acquire::http::Timeout=180 --option APT::Get::Install-Recommends=false --option APT::Get::Install-Suggests=false --yes update
+DEBIAN_FRONTEND=noninteractive apt-get --option Acquire::Retries=60 --option Acquire::http::Timeout=180 --option APT::Get::Install-Recommends=false --option APT::Get::Install-Suggests=false --yes install libssl-dev
+rm -rf /var/log/* /var/lib/apt/lists/* /var/cache/apt/archives/* /usr/share/doc /usr/share/man
