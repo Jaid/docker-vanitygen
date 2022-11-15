05a4503dcc2827c6b42d8852462f7357b9856eac
Jaid <jaid.jsx@gmail.com>
2022-11-15 04:20:11 +0100

    Improved health check
    Signed-off-by: Jaid <jaid.jsx@gmail.com>

--- src/healthCheck.bash
+++ src/healthCheck.bash
@@ -1,12 +1,9 @@
 #!/usr/bin/env bash
-set -o errexit -o pipefail
 
 declare -i pid
 pid=$(pgrep vanitygen++)
 declare -i cpuUsed
 cpuUsed=$(ps --pid "$pid" -o pcpu h)
-if [[ $cpuUsed -gt 0 ]]; then
-  exit 0
-else
+if [[ $cpuUsed -lt 10 ]]; then
   exit 1
 fi
