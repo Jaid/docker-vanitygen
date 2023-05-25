5e6130adef515a0393efde0a86fff060a39400bc
Jaid <jaid.jsx@gmail.com>
2023-05-25 07:19:21 +0200

    Suppressed exit code of vanitygen
    Signed-off-by: Jaid <jaid.jsx@gmail.com>

--- src/run.bash
+++ src/run.bash
@@ -65,7 +65,7 @@ function main {
     parsedThreads=$(parseThreads "$threads")
     arguments+=("$parsedThreads")
   fi
-  ~/bin/vanitygen++ "${arguments[@]}"
+  ~/bin/vanitygen++ "${arguments[@]}" || true
 }
 
 main 2> >(tee --append "$logFile")
